e8c513b5fb5adebdb7e26ccfd6448c0c5b59cfb3
Abderrahmen <abderrahmen@lepotcommun.fr>
2020-07-21 17:29:32 +0200

    setup updates

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,12 +3,11 @@ import css from './index.scss';
 import Button from '@components/button';
 import { withTranslation } from '@i18n';
 
-const Home = ({ t }) => {
-
+const Home = () => {
     return (
         <div className={css.home}>
             home page
-            <div>{t('common:test')} </div>
+            {/* <div>{t('common:test')} </div> */}
             <Button> </Button>
         </div>
     );
