54e7597978799fa5af605f810a14cff7d2c66f0e
AntoineSauvage <antoinesauvage.lpc@gmail.com>
2020-08-04 10:43:37 +0200

    Update button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import css from './Button.module.scss';
 
-interface Props {
-    children: React.ReactNode;
+interface ButtonProps {
+    children: string;
+    onClick: (event: React.MouseEvent<HTMLElement>) => void;
+    variant: string;
+    disabled?: boolean;
 }
 
-const Button = ({ children }: Props) => {
-    return <div>{children}</div>;
+const Button = ({ children, variant, disabled, onClick }: ButtonProps) => {
+    const style = () => {
+        let className = css.button;
+        variant === 'primary' && (className += ' ' + css.button__primary);
+        variant === 'secondary' && (className += ' ' + css.button__secondary);
+        disabled && (className += ' ' + css.button__disabled);
+
+        return className;
+    };
+
+    return (
+        <button className={style()} onClick={onClick}>
+            {children}
+        </button>
+    );
 };
 
 export default Button;
