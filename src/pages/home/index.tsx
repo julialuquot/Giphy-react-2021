11eebe64a37eccadf15f4ada7e955bc837a39496
AntoineSauvage <antoinesauvage.lpc@gmail.com>
2020-08-05 17:48:20 +0200

    Add message HP

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,8 @@ import css from './index.scss';
 import Button from '@components/common/Button/Button';
 import { withTranslation } from '@i18n';
 
+const namespacesRequired = ['common'];
+
 interface HomeProps {
     t: (string) => string;
 }
@@ -10,14 +12,24 @@ interface HomeProps {
 const Home = ({ t }: HomeProps) => {
     return (
         <div className={css.home}>
-            <h1>Titre H1</h1>
-            <h2>Titre H2</h2>
-            <h3>Titre H3</h3>
-            <h4>Titre H4</h4>
-            <div>{t('common:test')} </div>
-            <Button type="button" variant="brand-primary" size="medium" onClick={() => console.log('je click')}>
-                Home
-            </Button>
+            <h2>{t('common:test')}</h2>
+            <h4>Bon retour de vacances ^-^</h4>
+            <ul>
+                <li>Jai push tout mon travail sur develop, à savoir entre autre:</li>
+                <li>Ajout config sass module dans le dossier assets/styles.</li>
+                <li>Ajout exemple pour typescript avec interface.</li>
+                <li>
+                    Ajout config Storybook, qui permet la création dune librairie UI (tu pourras en parler avec Matt).
+                    Pour tester il faut lancer yarn storybook ou npm run storybook. La preview se fait dans le dossier
+                    /stories.
+                </li>
+                <li>
+                    Création des premiers composants surtout pour exemple et pour voir la meilleure méthodo ensemble.
+                    Les maquettes ne sont pas encore dispo sur zepplin à ce jour.
+                </li>{' '}
+                <li>Voilà , au plaisir de discuter avec toi de tout ça mardi quand je reviens ! :)</li>
+            </ul>
+
             <Button
                 type="button"
                 variant="brand-secondary"
@@ -25,11 +37,10 @@ const Home = ({ t }: HomeProps) => {
                 isLoading
                 onClick={() => console.log('je click')}
             >
-                Loading
+                Work in progress...
             </Button>
-            <div>hello</div>
         </div>
     );
 };
 
-export default withTranslation()(Home);
+export default withTranslation(namespacesRequired)(Home);
