0eda4d44d8120ed41be76b42de22176c28acb86f
AntoineSauvage <antoinesauvage.lpc@gmail.com>
2020-08-19 10:50:29 +0200

    Update select dropdown component

--- src/components/common/Select/Select.tsx
+++ src/components/common/Select/Select.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import css from './Select.module.scss';
 import Dropdown from 'react-dropdown';
+import useOnClickOutside from '../../Hooks/useOnClickOutside';
 
 type DropdownProps = {
     options: { value: string; label: string }[];
@@ -12,27 +13,57 @@ type DropdownProps = {
 const Select = ({ options, customClass, label, placeholder }: DropdownProps) => {
     const [selectedOption, setSelectedOption] = useState('');
     const [isOptionSelected, setIsOptionSelected] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const ref = useRef();
+    useOnClickOutside(ref, () => setIsOpen(false));
 
     const handleChange = (e) => {
         setSelectedOption(e.value);
         setIsOptionSelected(true);
+        setIsOpen(false);
     };
 
+    const handleFocus = () => {
+        setIsOpen(true);
+    };
+
+    const arrowClosed = (
+        <span className={css.select__arrow_closed}>
+            <img src="/icons/chevron-down-grey.svg" alt="chevron_down" />
+        </span>
+    );
+
+    const arrowOpen = (
+        <span className={css.select__arrow_open}>
+            {isOptionSelected ? (
+                <img src="/icons/chevron-up-white.svg" alt="chevron_up" />
+            ) : (
+                <img src="/icons/chevron-down-grey.svg" alt="chevron_up" />
+            )}
+        </span>
+    );
+
     return (
-        <div className={`${css.input__wrapper}  ${customClass || ''}}`}>
+        <div ref={ref} className={`${css.input__wrapper}  ${customClass || ''}}`}>
             <label className={css.label}>{label}</label>
             <Dropdown
                 options={options}
+                onFocus={handleFocus}
                 onChange={handleChange}
                 value={selectedOption}
                 placeholder={placeholder || options[0].label}
                 className={`${css.select}`}
-                controlClassName={`${css.select__control} ${isOptionSelected && css.select__control__option_selected}`}
-                placeholderClassName={`${css.select__placeholder}`}
+                controlClassName={`${css.select__control} ${
+                    isOpen && isOptionSelected && css.select__control__is_open
+                }`}
+                placeholderClassName={`${css.select__placeholder} ${
+                    isOptionSelected && css.select__placeholder__option_selected
+                }`}
                 menuClassName={css.select__menu}
-                arrowClassName={`${css.select__arrow} ${
-                    !isOptionSelected ? css.select__arrow__default : css.select__arrow__option_selected
-                } `}
+                arrowClassName={`${css.select__arrow}`}
+                arrowClosed={arrowClosed}
+                arrowOpen={arrowOpen}
             />
         </div>
     );
