4971be0ca9bcddb606f347db895c63b36c2fff4c
Abderrahmen <abderrahmen@lepotcommun.fr>
2020-09-10 16:49:09 +0200

    updates

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Navbar from '@components/navbar/Navbar';
-import Footer from '@components/footer/Footer';
+import Navbar from '../navbar/Navbar';
+import Footer from '../footer/Footer';
 import css from './Layout.scss';
 
 type LayoutProps = {
