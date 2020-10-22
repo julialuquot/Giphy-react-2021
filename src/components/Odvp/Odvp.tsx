c0ab0a0d7ab4575b37b658fb20ce2a03ab0484a5
AntoineSauvage <antoinesauvage.lpc@gmail.com>
2020-10-22 12:02:41 +0200

    Add stiky button WIP

--- src/components/Odvp/Odvp.tsx
+++ src/components/Odvp/Odvp.tsx
@@ -1,18 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import css from './Odvp.module.scss';
-import Nav from '@components/Odvp/Nav/Nav';
 import Reindeer from '@components/Odvp/01_Reindeer/Reindeer';
 import MeanWise from '@components/Odvp/02_MeanWise/MeanWise';
 import Balls from '@components/Odvp/03_Balls/Balls';
 import Scene from '@components/Odvp/04_Scene/Scene';
 import Bottom from '@components/Odvp/05_Bottom/Bottom';
+import OdvbButton from '@components/Odvp/OdvbButton/OdvpButton';
+import OdvpButton from '@components/Odvp/OdvbButton/OdvpButton';
 
 const Odvp = () => {
     const [showNavOnScroll, setShowNavOnScroll] = useState(false);
 
+    const [isUserScrolling, setIsUserScrolling] = useState(false);
+
+    let isScrolling;
+
+    const handleScroll = () => {
+        setIsUserScrolling(true);
+        // Clear our timeout throughout the scroll
+        window.clearTimeout(isScrolling);
+        // Set a timeout to run after scrolling ends
+        isScrolling = setTimeout(function () {
+            setIsUserScrolling(false);
+        }, 1000);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [handleScroll]);
+
     return (
         <>
-            {/* <Nav isVisible={showNavOnScroll} /> */}
+            {/* <Nav isVisible={showNavOnScroll} /> */}{' '}
+            <div className={`${css.button} ${isUserScrolling && css.button__hide}`}>
+                <OdvbButton width={200} height={47}>
+                    Créer un pot commun
+                </OdvbButton>
+            </div>
             <Reindeer onShowNavBar={(value) => setShowNavOnScroll(value)} />
             <MeanWise />
             <Balls />
