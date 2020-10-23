63f34d07007d5017678d27d4195f62994ac79750
AntoineSauvage <antoinesauvage.lpc@gmail.com>
2020-10-23 11:46:06 +0200

    Add Cat animation and update Scene component Layout

--- src/components/Odvp/04_Scene/Scene.tsx
+++ src/components/Odvp/04_Scene/Scene.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import css from './Scene.module.scss';
+import Lottie from 'react-lottie';
+import cat from '../../../../public/icons/odvp/lottie/Cat.json';
+
+const catOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: cat,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+    },
+};
 
 const Scene = () => {
     return (
@@ -11,6 +22,9 @@ const Scene = () => {
                     <span className={css.scene__text__bold}> les heureux gagnants !</span>
                 </p>
             </div>
+            <figure className={css.scene__cat}>
+                <Lottie options={catOptions} width={211} height={224} />
+            </figure>
         </div>
     );
 };
