a9a6ee5db04cf5607ee3b0a22ac678567a95f576
Abderrahmen <abderrahmen@lepotcommun.fr>
2020-12-02 12:35:09 +0100

    move fonts to front-static and update code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,14 +6,14 @@ class MyDocument extends Document {
         return (
             <Html lang="fr">
                 <Head />
-                <link rel="preload" href="/fonts/Poppins-Regular.woff2" as="font" crossOrigin="" />
-                <link rel="preload" href="/fonts/Poppins-Bold.woff2" as="font" crossOrigin="" />
-                <link rel="preload" href="/fonts/Poppins-BoldItalic.woff2" as="font" crossOrigin="" />
-                <link rel="preload" href="/fonts/Poppins-Italic.woff2" as="font" crossOrigin="" />
-                <link rel="preload" href="/fonts/Poppins-Medium.woff2" as="font" crossOrigin="" />
-                <link rel="preload" href="/fonts/Poppins-MediumItalic.woff2" as="font" crossOrigin="" />
-                <link rel="preload" href="/fonts/Poppins-SemiBold.woff2" as="font" crossOrigin="" />
-                <link rel="preload" href="/fonts/Poppins-SemiBoldItalic.woff2" as="font" crossOrigin="" />
+                <link rel="preload" href="/front-static/fonts/Poppins-Regular.woff2" as="font" crossOrigin="" />
+                <link rel="preload" href="/front-static/fonts/Poppins-Bold.woff2" as="font" crossOrigin="" />
+                <link rel="preload" href="/front-static/fonts/Poppins-BoldItalic.woff2" as="font" crossOrigin="" />
+                <link rel="preload" href="/front-static/fonts/Poppins-Italic.woff2" as="font" crossOrigin="" />
+                <link rel="preload" href="/front-static/fonts/Poppins-Medium.woff2" as="font" crossOrigin="" />
+                <link rel="preload" href="/front-static/fonts/Poppins-MediumItalic.woff2" as="font" crossOrigin="" />
+                <link rel="preload" href="/front-static/fonts/Poppins-SemiBold.woff2" as="font" crossOrigin="" />
+                <link rel="preload" href="/front-static/fonts/Poppins-SemiBoldItalic.woff2" as="font" crossOrigin="" />
                 <link
                     rel="stylesheet"
                     type="text/css"
