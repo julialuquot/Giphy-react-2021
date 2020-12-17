40ac0d3d939aad51ea8e44788ee41cc1a2227e0c
AntoineSauvage <antoinesauvage.lpc@gmail.com>
2020-12-17 10:02:15 +0100

    Fix header avatar with logic for merchant and admin.

--- src/components/dashboard/Header/Header.tsx
+++ src/components/dashboard/Header/Header.tsx
@@ -1,91 +1,101 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import css from './Header.module.scss';
+import { useRouter } from 'next/router';
 import Avatar from '@components/dashboard/Header/Avatar/Avatar';
 import AuthContext from '@components/dashboard/context/auth/AuthContext';
 import Profil from '@components/dashboard/Header/Profil/Profil';
 import useOnClickOutside from '@components/common/hooks/useOnClickOutside';
 import { getRoute, ROUTE } from '@services/http/Route';
-import { useRouter } from 'next/router';
-import InformationsService from '@services/domain/InformationsService';
+import AuthService from '@services/domain/AuthService';
+import css from './Header.module.scss';
+import InformationsContext from '@components/dashboard/context/informations/InformationsContext';
 
 const Header = () => {
     const router = useRouter();
-    const [brand, setBrand] = useState(null);
-
-    const authContext = useContext(AuthContext);
-    const { userSignOut, isFetching, getUser, user } = authContext;
-
-    const [isProfilOpen, setIsProfilOpen] = useState(false);
-
     const avatarRef = useRef();
     useOnClickOutside(avatarRef, () => setIsProfilOpen(false));
 
-    useEffect(() => {
-        getUser();
-    }, [getUser]);
+    const authContext = useContext(AuthContext);
+    const { userSignOut, getUser, user } = authContext;
 
-    useEffect(() => {
-        user && InformationsService.getBrand(user.partnerUniq).then((res) => setBrand(res.data));
-    }, [user]);
+    const informationsContext = useContext(InformationsContext);
+    const { getBrand, brand, isFetching } = informationsContext;
+
+    const [isProfilOpen, setIsProfilOpen] = useState(false);
+    const [userRole, setUserRole] = useState(null);
+    const [merchantInfos, setMerchantInfos] = useState(null);
+    const [adminInfos, setAdminInfos] = useState(null);
+
+    const onToggleProfil = () => {
+        setIsProfilOpen(!isProfilOpen);
+    };
 
     const onSignOut = async () => {
         await userSignOut();
         await router.push(getRoute(ROUTE.DASHBOARD.SIGN_IN, null));
     };
 
-    const getInitial = () => {
-        if (user && brand && brand.name && brand.name !== '') {
-            return brand.name.charAt(0).toUpperCase();
-        }
+    useEffect(() => {
+        getUser();
+    }, [getUser]);
 
-        return '';
-    };
+    useEffect(() => {
+        user && setUserRole(AuthService.getUserRole(user));
+    }, [user]);
 
-    const getColor = () => {
-        if (user && brand && brand.color && brand.color !== '') {
-            return brand.color;
+    useEffect(() => {
+        if (user && userRole === 'ADMIN') {
+            const adminName = user?.email;
+            const adminInitial = adminName.charAt(0).toUpperCase();
+            const values = {
+                name: adminName,
+                initial: adminInitial,
+                color: '#E22B76',
+            };
+            return setAdminInfos(values);
         }
-    };
+    }, [user, userRole]);
 
-    const getName = () => {
-        if (brand && brand.name !== '') {
-            return brand.name;
+    useEffect(() => {
+        if (user && userRole === 'MERCHANT') {
+            getBrand(user.partnerUniq);
+            const merchantName = brand?.name ? brand.name : user?.email;
+            const merchantInitial = merchantName ? merchantName.charAt(0).toUpperCase() : '';
+            const merchantColor = brand?.color ? brand.color : '#E22B76';
+            const values = {
+                name: merchantName,
+                initial: merchantInitial,
+                color: merchantColor,
+                brand: brand?.brand,
+            };
+            return setMerchantInfos(values);
         }
-        if (user && user.email) {
-            return user.email;
-        }
-    };
-
-    const onToggleProfil = () => {
-        setIsProfilOpen(!isProfilOpen);
-    };
+    }, [brand?.brand, brand.color, brand?.name, getBrand, user, userRole]);
 
     return (
         <nav className={css.navbar}>
             <div className={css.navbar__left}>
                 <img className={css.navbar__logo} src="/front-static/icons/logo-lpc.svg" alt={'logo lpc'} />
             </div>
-            {!isFetching && user && (
+            {!isFetching && (merchantInfos?.initial || adminInfos?.initial) && (
                 <div ref={avatarRef}>
                     <Avatar
                         onToggleProfil={() => onToggleProfil()}
-                        initial={getInitial()}
-                        name={getName()}
-                        color={getColor()}
+                        initial={merchantInfos?.initial || adminInfos?.initial}
+                        name={merchantInfos?.name || adminInfos?.name}
+                        color={merchantInfos?.color || adminInfos?.color}
                         height={'38px'}
                         width={'38px'}
                     />
                     <Profil
                         onSignOut={() => onSignOut()}
                         isOpen={isProfilOpen}
-                        initial={getInitial()}
-                        name={getName()}
-                        color={getColor()}
+                        initial={merchantInfos?.initial || adminInfos?.initial}
+                        name={merchantInfos?.name || adminInfos?.name}
+                        color={merchantInfos?.color || adminInfos?.color}
                     />
                 </div>
             )}
         </nav>
     );
 };
-
 export default Header;
